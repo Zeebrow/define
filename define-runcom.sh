0c0deed801db73549603dbff98f082f68e4e6e2d
Zeebrow <mzborowski@yahoo.com>
2022-10-22 22:40:40 -0400

    fix ignored flags by runcom

--- define-runcom.sh
+++ define-runcom.sh
@@ -3,6 +3,9 @@ mkdir -p "$XDG_DATA_HOME/define"
 DEFINED_WORDS="$XDG_DATA_HOME/define/defined_words.txt"
 [ ! -f "$DEFINED_WORDS" ] && touch "$DEFINED_WORDS"
 
+# skip if first arg is a flag
+[ "${1:0:1}" == '-' ] && define "$@" && exit
+
 define "$@"
 rc=$?
 if [ "$rc" -eq 0 ]; then 
